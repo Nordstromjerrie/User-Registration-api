7b6580766832ff6cf3229d236ad62d22217b6c32
jerrie <Jerrie.77@hotmail.se>
2026-09-09 09:49:07 +0200

    comment assert that failed test

--- src/test/java/se/testkurs/userapi/service/UserServiceTest.java
+++ src/test/java/se/testkurs/userapi/service/UserServiceTest.java
@@ -159,7 +159,7 @@ class UserServiceTest {
             List <User> result = userService.getAllUsers();
             assertEquals(user, result.getFirst()); //Best praxis for assertEquals in this case
             assertEquals(user.getUsername(), result.getFirst().getUsername()); // Only test one field and leave to much
-            assertEquals("User{id=1L, username='Jerran', email='Test@test.com'}", result.getFirst()); // Worst case. Does not follow "dont test implentation details" Hard codede strings is too sensitive and break easy.
+           // assertEquals("User{id=1L, username='Jerran', email='Test@test.com'}", result.getFirst()); // Worst case. Does not follow "dont test implentation details" Hard codede strings is too sensitive and break easy.
             verify(userRepository).findAll();
         }
 
