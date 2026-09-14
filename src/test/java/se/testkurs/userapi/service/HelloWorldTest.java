e9cea77b4b2cfa8fd996adc97cc4755a84e1d70d
jerrie <Jerrie.77@hotmail.se>
2026-09-14 11:32:04 +0200

    Created HelloWorldTest Class, demonstrate failing test report

---
+++ src/test/java/se/testkurs/userapi/service/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package se.testkurs.userapi.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+    @Test
+    void additionIsCorrect(){
+        assertEquals(5, 2+2);
+
+    }
+}
